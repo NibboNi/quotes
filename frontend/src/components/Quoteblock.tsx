e43a03797025c58dc838a69872953555851d807c
nibbo <131214845+NibboNi@users.noreply.github.com>
2025-09-14 00:49:46 -0600

    feat: add Quoteblock component

--- frontend/src/components/Quoteblock.tsx
+++ frontend/src/components/Quoteblock.tsx
@@ -1,15 +1,14 @@
-interface QuoteblockProps {
-  quote: string;
-  quoted: string;
-}
+import type { Quote } from "../types";
 
-function Quoteblock({ quote, quoted }: QuoteblockProps) {
+type QuoteblockProps = Pick<Quote, "content" | "author">;
+
+function Quoteblock({ content, author }: QuoteblockProps) {
   return (
     <section className="h-full mx-auto w-11/12 max-w-6xl flex flex-col justify-center items-center gap-5">
       <h2 className="text-4xl font-thin text-center md:text-5xl lg:text-6xl xl:text-7xl">
-        {quote}
+        {content}
       </h2>
-      <p className="text-xl font-light md:ml-auto md:text-lg">- {quoted}</p>
+      <p className="text-xl font-light md:ml-auto md:text-lg">- {author}</p>
     </section>
   );
 }
